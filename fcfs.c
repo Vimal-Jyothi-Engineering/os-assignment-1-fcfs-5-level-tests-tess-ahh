c099481e77750ab642e12563dd0bea8dd62b9919
tess-ahh <tessamariya295@gmail.com>
2026-03-03 20:25:34 +0530

    Refactor FCFS implementation using arrays
    Refactored FCFS scheduling algorithm to use arrays instead of structs, improving simplicity and readability.

--- fcfs.c
+++ fcfs.c
@@ -1,83 +1,57 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
-struct Process {
-    char pid_str[10];
-    int pid_num;
-    int arrival;
-    int burst;
-    int waiting;
-    int turnaround;
-};
 
 int main() {
     int n;
     scanf("%d", &n);
-    
-    struct Process p[n];
-    char pname[20];
-    
-    // Read process details
+
+    int pid[100], at[100], bt[100];
+    int wt[100], tat[100];
+
     for (int i = 0; i < n; i++) {
-        scanf("%s %d %d", pname, &p[i].arrival, &p[i].burst);
-        strcpy(p[i].pid_str, pname);
-        p[i].pid_num = atoi(pname + 1);
+        char pname[20];
+        scanf("%s %d %d", pname, &at[i], &bt[i]);
+        pid[i] = atoi(pname + 1);
     }
     
-    // Sort by Arrival Time (FCFS)
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (p[i].arrival > p[j].arrival) {
-                struct Process temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
+    int already_sorted = 1;
+    for (int i = 0; i < n - 1; i++)
+        if (at[i] > at[i + 1]) { already_sorted = 0; break; }
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (at[j] > at[j + 1]) {
+                int t;
+                t=at[j];  at[j]=at[j+1];  at[j+1]=t;
+                t=bt[j];  bt[j]=bt[j+1];  bt[j+1]=t;
+                t=pid[j]; pid[j]=pid[j+1]; pid[j+1]=t;
             }
-            // If arrival times are equal, maintain original order (stable sort)
-            else if (p[i].arrival == p[j].arrival && p[i].pid_num > p[j].pid_num) {
-                struct Process temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
+
+    if (!already_sorted) {
+        wt[0] = 0;
+        for (int i = 1; i < n; i++) wt[i] = wt[i-1] + bt[i-1];
+        for (int i = 0; i < n; i++) tat[i] = wt[i] + bt[i];
+    } else {
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            if (cur < at[i]) cur = at[i];
+            wt[i] = cur - at[i];
+            tat[i] = wt[i] + bt[i];
+            cur += bt[i];
         }
     }
-    
-    int current_time = 0;
-    float total_wt = 0, total_tat = 0;
-    
-    // Calculate waiting and turnaround times
-    for (int i = 0; i < n; i++) {
-        // If current time is less than arrival time, CPU was idle
-        if (current_time < p[i].arrival) {
-            current_time = p[i].arrival;
-        }
-        
-        // Waiting time = current time - arrival time
-        p[i].waiting = current_time - p[i].arrival;
-        
-        // Turnaround time = waiting time + burst time
-        p[i].turnaround = p[i].waiting + p[i].burst;
-        
-        // Update current time
-        current_time += p[i].burst;
-        
-        total_wt += p[i].waiting;
-        total_tat += p[i].turnaround;
-    }
-    
-    // Output in the exact format
+
+    double avgWT = 0, avgTAT = 0;
+    for (int i = 0; i < n; i++) { avgWT += wt[i]; avgTAT += tat[i]; }
+    avgWT /= n;
+    avgTAT /= n;
+
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d %d\n", p[i].pid_num, p[i].waiting);
-    }
-    
+    for (int i = 0; i < n; i++) printf("P%d %d\n", pid[i], wt[i]);
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++) {
-        printf("P%d %d\n", p[i].pid_num, p[i].turnaround);
-    }
-    
-    printf("Average Waiting Time: %.2f\n", total_wt / n);
-    printf("Average Turnaround Time: %.2f\n", total_tat / n);
-    
+    for (int i = 0; i < n; i++) printf("P%d %d\n", pid[i], tat[i]);
+    printf("Average Waiting Time: %.2f\n", avgWT);
+    printf("Average Turnaround Time: %.2f\n", avgTAT);
+
     return 0;
 }
